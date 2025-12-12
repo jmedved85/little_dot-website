05db387dcc671544ba705742139c9818acc0f55f
Jasmin Medved <jmedved85@gmail.com>
2025-12-12 17:40:43 +0100

    deprecation fix

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,7 +13,7 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class UserController extends AbstractController
